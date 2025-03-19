e7258466f108a08f4fc29dc414f0d92ef833b77b
Muhamed <110548351+oop7@users.noreply.github.com>
2025-03-19 11:44:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=[
         "requests",
         "tiktoken",
