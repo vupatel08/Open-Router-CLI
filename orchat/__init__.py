f42585000916d5ed51dd5562d6c2a2f207c5e4b3
Muhamed <m7967483@gmail.com>
2025-03-19 10:24:58 +0200

    Prepare package for PyPI publishing

---
+++ orchat/__init__.py
@@ -0,0 +1,3 @@
+"""OrChat - A powerful CLI for AI models via OpenRouter"""
+
+__version__ = "1.0.0"
---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="orchat",
+    version="1.0.0",
+    author="oop7",
+    author_email="oop7_support@proton.me",
+    description="A powerful CLI for chatting with AI models through OpenRouter",
+    long_description=long_description,
+    long_description=Path('README.md').read_text(encoding='utf-8'),
+    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
+    url="https://github.com/oop7/OrChat",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "requests",
+        "tiktoken",
+        "rich",
+        "python-dotenv", 
+        "colorama",
+        "packaging",
+    ],
+    entry_points={
+        "console_scripts": [
+            "orchat=orchat.main:main",
+        ],
+    },
+    project_urls={
+        'Homepage': 'https://github.com/oop7/YTSage',
+        'Bug Tracker': 'https://github.com/oop7/YTSage/issues',
+        'Reddit': 'https://www.reddit.com/r/NO-N_A_M_E/',
+    },
+)
